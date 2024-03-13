57303e69ed73b157e74eda179b708662a43d789e
Shiyang Yan <46278468+Shiyang-Yan@users.noreply.github.com>
2024-03-13 15:16:04 +0800

    Update stage2_net.py

--- models/stage2_net.py
+++ models/stage2_net.py
@@ -7,9 +7,10 @@ from collections import OrderedDict
 
 class Temporal_I3D_SGA_STD(nn.Module):
     def __init__(self,dropout_rate):
-
-        super(Temporal_I3D_SGA_STD, self).__init__()
-        self.Regressor=nn.Sequential(nn.Linear(4096,512),nn.ReLU(), nn.Dropout(dropout_rate),nn.Linear(512, 32),nn.Dropout(dropout_rate),nn.Linear(32, 2))
+        super(Temporal_I3D_SGA_STD, self).__init__(k=1, soft_voting=True)
+        self.k = k
+        self.soft_voting = soft_voting
+        self.Regressor=[nn.Sequential(nn.Linear(4096,512),nn.ReLU(), nn.Dropout(dropout_rate),nn.Linear(512, 32),nn.Dropout(dropout_rate),nn.Linear(32, 2)) for i in range(self.k)]
         self.Softmax=nn.Softmax(dim=-1)
 
     def train(self,mode=True):
@@ -17,8 +18,25 @@ class Temporal_I3D_SGA_STD(nn.Module):
         return self
 
     def forward(self,x,act=True,extract=False):
-        logits=self.Regressor(x)
-        if act:
-            logits=self.Softmax(logits)
-        return logits
+        logits_all = []
+        for i in range(self.k):
+            logits=self.Regressor[i](x)
+            if act:
+                logits=self.Softmax(logits)
+            logits_all.append(logits)
+        if self.soft_voting:
+            logits_all = torch.stack(logits_all, 1)  #### should be "dimension Batchsize, 2, k"
+            logits_final = logits_all.mean(-1)
+            pseudo_labels = torch.argmax(logits_final, 1)
+        else:
+            result_all = []
+            zero_num = torch.zeros([x.size(0)]).cuda()
+            one_num = torch.zeros([x.size(0)]).cuda()
+            for i in range(self.k):
+                result = torch.argmax(logits_all[i], 1)  ####, batchsize, 2
+                zero_num += (result==0)   #### batchsize, 1
+                one_num += (result==1)    #### batchsize, 1
+            nums = torch.cat([zero_num, one_num], -1)    #### batchsize, 2
+            pseudo_labels = torch.argmax(nums, 1)
+        return pseudo_labels
 
